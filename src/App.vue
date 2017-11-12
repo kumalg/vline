873bab827aed0255ebe68099833cd641dae8e75b
kumalg <kumalgfilms@gmail.com>
2017-11-12 16:12:54 +0100

    facebook + oferta

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,93 @@
 <template>
-  <div id="app">
-    <div id="menu" ref="menu" class="m1 menu" :class="[active, {'menu-splash': isSplashVisible}]">
-      <scrollactive
-          id="menu-center"
-          :offset="96"
-          v-on:itemchanged="yourFunction()">
-        <a id="logo-button" class="scrollactive-item logo-button splash" href="#splash"></a>
-        <ul class="menu-sections">
-          <li v-for="item in menuItems">
-            <a :href="'#' + item.name" :class="item.name" class="scrollactive-item">{{item.title}}</a>
-          </li>
-        </ul>
-      </scrollactive>
-    </div>
-
-    <section id="splash" ref="splash">
-      <h6>Internet + Telewizja + Telefon</h6>
-    </section>
-
-    <section id="o_vline" ref="o_vline">
-    </section>
-
-    <section id="oferta" ref="oferta">
-    </section>
-
-    <section id="promocje" ref="oferta">
-    </section>
-
-    <section id="aktualnosci" ref="aktualnosci">
-    </section>
-
-    <section id="kontakt" ref="kontakt">
-    </section>
+<div id="app">
+  <div id="menu" ref="menu" class="m1 menu" :class="{'menu-splash': isSplashVisible}">
+    <scrollactive id="menu-center" :offset="96">
+      <a id="logo-button" class="scrollactive-item logo-button splash" href="#splash"></a>
+      <ul class="menu-sections">
+        <li v-for="item in menuItems">
+          <a :href="item.href" :class="{'scrollactive-item': !item.external}">{{item.title}}</a>
+        </li>
+      </ul>
+    </scrollactive>
   </div>
+
+  <section id="splash" ref="splash">
+    <img src="./assets/logo.png" id="logo" />
+    <h6>Internet &nbsp;&nbsp; + &nbsp;&nbsp; Telewizja &nbsp;&nbsp; + &nbsp;&nbsp; Telefon</h6>
+  </section>
+
+  <section id="o_vline" ref="o_vline">
+  </section>
+
+  <section id="oferta" ref="oferta">
+    <div class="container">
+      <h1 class="heading">Oferta</h1>
+      <ul>
+        <li>
+          <a class="active">Internet</a>
+        </li>
+        <li>
+          <a>Telewizja</a>
+        </li>
+        <li>
+          <a>Telefon</a>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <section id="promocje" ref="promocje">
+    <div class="container">
+      <h1 class="heading">Promocje</h1>
+    </div>
+  </section>
+
+  <Aktualnosci id="aktualnosci" ref="aktualnosci"/>
+
+  <section id="kontakt" ref="kontakt">
+  </section>
+</div>
 </template>
 
 <script>
+import Aktualnosci from './components/Aktualnosci'
 
 export default {
   name: 'app',
   data() {
     return {
       isSplashVisible: true,
-      active: 'splash',
       scrollPosition: null,
-      menuItems: [
-        {name:'o_vline', title: 'O VLine'},
-        {name:'oferta', title: 'Oferta'},
-        {name:'promocje', title: 'Promocje'},
-        {name:'aktualnosci', title: 'Aktualności'},
-        {name:'kontakt', title: 'Kontakt'}
+      menuItems: [{
+          href: '#o_vline',
+          title: 'O VLine',
+        },
+        {
+          href: '#oferta',
+          title: 'Oferta'
+        },
+        {
+          href: '#promocje',
+          title: 'Promocje'
+        },
+        {
+          href: '#aktualnosci',
+          title: 'Aktualności'
+        },
+        {
+          href: '#kontakt',
+          title: 'Kontakt'
+        },
+        {
+          href: 'https://poczta.vline.net.pl/src/login.php',
+          external: true,
+          title: 'Panel klienta'
+        }
       ]
     }
   },
   components: {
+    Aktualnosci
   },
   methods: {
     // makeActive(item){
@@ -62,10 +96,7 @@ export default {
     //   this.active = itemName
     // },
     updateMenuStyle() {
-      this.isSplashVisible = this.scrollPosition <  this.$refs.splash.offsetTop + this.$refs.splash.offsetHeight - this.$refs.menu.offsetHeight
-    },
-    yourFunction(){
-
+      this.isSplashVisible = this.scrollPosition < this.$refs.splash.offsetTop + this.$refs.splash.offsetHeight - this.$refs.menu.offsetHeight
     },
     updateScroll() {
       this.scrollPosition = window.scrollY
@@ -78,9 +109,11 @@ export default {
   mounted() {
     window.addEventListener('scroll', this.updateScroll);
     window.addEventListener('resize', this.updateResize);
+  },
+  created: function () {
+    updateMenuStyle()
   }
 }
-
 </script>
 
 <style src="./styles/reset.css"></style>
@@ -104,8 +137,7 @@ html {
 #app {
   // font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
+  -moz-osx-font-smoothing: grayscale; // text-align: center;
   // color: #2c3e50;
   // margin-top: 60px;
 }
@@ -138,6 +170,10 @@ html {
   }
 }
 
+#logo {
+  margin-bottom: 80px;
+}
+
 #o_vline {
   background-color: #ebebeb;
   height: 100vh;
@@ -148,16 +184,46 @@ html {
   background-color: #fff;
   height: 100vh;
   width: 100%;
+
+  ul {
+    margin: 32px 0 64px 0;
+    text-align: center;
+
+    li {
+      list-style: none;
+      margin: 0 8px;
+      display: inline;
+      cursor: pointer;
+
+      a {
+        padding: 0 32px;
+        height: 48px;
+        display: inline-block;
+        font-weight: 600;
+        font-size: 0.875em;
+        color: $colorPrimary;
+        text-decoration: none;
+        line-height: 44px;
+        transition: background-color 0.2s, color 0.2s;
+        border: {
+          style: solid;
+          radius: 24px;
+          width: 2px;
+          color: $colorPrimary;
+        }
+
+        &.active,
+        &:hover {
+          color: white !important;
+          background-color: $colorPrimary !important;
+        }
+      }
+    }
+  }
 }
 
 #promocje {
   background-color: #ebebeb;
-  height: 640px;
-  width: 100%;
-}
-
-#aktualnosci {
-  background-color: #fff;
   height: 640px;
   width: 100%;
 }
@@ -168,4 +234,16 @@ html {
   width: 100%;
 }
 
+.container {
+  max-width: 960px;
+  padding-top: 80px;
+  margin: 0 auto;
+}
+
+.heading {
+  font-size: 2.25em;
+  font-weight: 300;
+  color: $colorPrimary;
+  text-align: center;
+}
 </style>
---
+++ src/components/Aktualnosci.vue
@@ -0,0 +1,46 @@
+<template>
+<section>
+  <div class="container">
+    <h1 class="heading">Aktualności</h1>
+    <div class="fb-posts">
+      <div v-for="link in fbPostLinks" class="fb-post" :data-href="link" data-width="300"></div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+import Links from '../api/fbPostLinks.json'
+
+export default {
+  name: 'Aktualnosci',
+  data () {
+    return {
+      fbPostLinks: Links
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+section {
+  background-color: #fff;
+  min-height: 640px;
+  width: 100%;
+
+  .container {
+      max-width: 1100px;
+  }
+
+  .fb-posts {
+    margin: 32px 0 56px 0;
+    text-align: center;
+
+    * {
+     margin: 0 8px;
+     vertical-align: top;
+     margin-bottom: 16px;
+    }
+  }
+}
+</style>
