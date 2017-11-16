5aa8660a8509c4d6ad87f6f4547adbe9b9a0261c
kumalg <kumalgfilms@gmail.com>
2017-11-17 00:07:41 +0100

    offer files and fixes

--- src/components/Offer_Internet.vue
+++ src/components/Offer_Internet.vue
@@ -1,22 +1,20 @@
 <template>
-<div>
-  <div class="nav">
-    <div class="title">
-      Długość trwania oferty:
+<div class="container">
+  <div class="container_inner">
+    <div class="nav">
+      <h1>Długość trwania oferty:</h1>
+      <ul>
+        <li v-for="(item, index) in items">
+          <button :class="{'active': actualIndex == index}" @click="agreementLengthItemClick(index)">
+            <div class="dot"/><p>
+              {{item.agreement_length}}
+            </p>
+          </button>
+        </li>
+      </ul>
     </div>
-    <ul>
-      <li v-for="(item, index) in items">
-        <button :class="{'active': actualIndex == index}" @click="agreementLengthItemClick(index)">
-          <div class="dot"/><p>
-            {{item.agreement_length}}
-          </p>
-        </button>
-      </li>
-    </ul>
-  </div>
-  <div class="items">
     <transition name="offer-fade" mode="out-in">
-      <div v-bind:key="actualIndex">        
+      <div v-bind:key="actualIndex" class="items">
         <div class="item list-item" v-for="item in items[actualIndex].items">
           <div class="title">
             {{item.title}}
@@ -24,7 +22,7 @@
           <div class="subtitle">
             {{item.subtitle}}
           </div>
-          <div class="more-info" v-if="item.moreInfo != null">
+          <div class="more-info" v-if="item.moreInfo">
             <div v-for="info in item.moreInfo">
               {{info}}
             </div>
@@ -35,6 +33,16 @@
         </div>
       </div>
     </transition>
+  </div>
+  <div class="files" v-if="links">
+    <h1>Do pobrania:</h1>
+    <ul>
+      <li v-for="link in links">
+        <a :href="link.href">
+          <i class="fa fa-file-pdf-o icon" aria-hidden="true"></i><p>{{link.title}}</p>
+        </a>
+      </li>
+    </ul>
   </div>
 </div>
 </template>
@@ -47,7 +55,8 @@ export default {
   data() {
     return {
       actualIndex: 0,
-      items: []
+      items: null,
+      links: null,
     }
   },
   created: function() {
@@ -66,6 +75,13 @@ export default {
         .catch(function(error) {
           console.log(error);
         });
+      axios.get('/static/api/offer_internet_links.json')
+        .then(function(resp) {
+          self.links = resp.data
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
   }
 }
@@ -74,42 +90,67 @@ export default {
 <style lang="scss" scoped>
 @import '../styles/_colors.scss';
 
-div {
+.files h1,
+.nav h1 {
+    font-size: 1.125em;
+    color: $colorPrimary;
+    font-family: inherit;
+    // font-family: 'Open Sans';
+}
+
+.files {
+    margin-top: 16px;
+
+    ul {
+        margin-top: 16px;
+        li {
+            line-height: 32px;
+            .icon {
+                color: $colorPrimary;
+                font-size: 1.125em;
+            }
+            p {
+                padding-left: 16px;
+                font-size: 0.75em;
+                font-weight: 600;
+                display: inline;
+                color: black;
+            }
+        }
+    }
+}
+
+.container_inner {
+    // background-color: blue;
     display: flex;
     width: 100%;
 
+    .offer-fade-enter-active,
+    .offer-fade-leave-active {
+        transition: all 0.2s ease;
+    }
+    /* .component-fade-leave-active below version 2.1.8 */
+    .offer-fade-enter,
+    .offer-fade-leave-to {
+        opacity: 0;
+    }
 
-        .offer-fade-enter-active,
-        .offer-fade-leave-active {
-            transition: all 0.2s ease;
-        }
-        /* .component-fade-leave-active below version 2.1.8 */
-        .offer-fade-enter,
-        .offer-fade-leave-to {
-            opacity: 0;
-        }
+    .offer-fade-enter {
+        transform: translateX(8px);
+        // transform: scale(0.98, 0.98);
+    }
 
-        .offer-fade-enter {
-            transform: translateX(8px);
-            // transform: scale(0.98, 0.98);
-        }
-
-        .offer-fade-leave-to {
-            transform: translateX(-8px);
-            // transform: scale(1.02, 1.02);
-        }
+    .offer-fade-leave-to {
+        transform: translateX(-8px);
+        // transform: scale(1.02, 1.02);
+    }
 
     .nav {
         flex: 1;
         min-width: 210px;
-        flex-direction: column;
+        // flex-direction: column;
         padding-bottom: 16px;
-
-        .title {
-            font-size: 1.125em;
-            color: $colorPrimary;
-            font-family: inherit;
-        }
+        // background: blue;
 
         ul {
             margin-top: 16px;
@@ -152,12 +193,12 @@ div {
         display: flex;
         flex-flow: row wrap;
         justify-content: flex-start;
+        // background-color: red;
 
         .item {
-            width: 31%;
+            width: calc(33.33% - 16px);
             min-width: 192px;
             padding: 32px;
-            flex-direction: column;
             margin: 0 8px 16px;
             text-align: center;
             border: {
@@ -165,10 +206,6 @@ div {
                 width: 1px;
                 radius: 3px;
                 color: rgba(0,0,0,.25);
-            }
-
-            * {
-                display: block;
             }
 
             .title {
@@ -205,18 +242,26 @@ div {
 
     @media all and (max-width: 992px) {
         .item {
-            width: 45% !important;
+            width: calc(50% - 16px) !important;
         }
     }
 
     @media all and (max-width: 700px) {
         .item {
-            width: 95% !important;
+            width: calc(100% - 16px) !important;
         }
     }
 
     @media all and (max-width: 500px) {
         flex-direction: column;
+
+        .item {
+            width: 100% !important;
+            margin: {
+                left: 0 !important;
+                right: 0 !important;
+            }
+        }
 
         .nav {
             text-align: center;
