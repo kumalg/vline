574d276ef9ab1ca2d1ea271fe8308f13215e419f
kumalg <kumalgfilms@gmail.com>
2017-11-17 13:55:04 +0100

    og:image fix

--- src/App.vue
+++ src/App.vue
@@ -119,28 +119,28 @@ export default {
   directives: {
     ClickOutside
   },
-  head: {
-   meta: [
-     // { name: 'application-name', content: 'Name of my application' },
-     // { name: 'description', content: 'A description of the page', id: 'desc' }, // id to replace intead of create element
-     // // ...
-     // // Twitter
-     // { name: 'twitter:title', content: 'Content Title' },
-     // // with shorthand
-     // { n: 'twitter:description', c: 'Content description less than 200 characters'},
-     // // ...
-     // // Google+ / Schema.org
-     // { itemprop: 'name', content: 'Content Title' },
-     // { itemprop: 'description', content: 'Content Title' },
-     // // ...
-     // // Facebook / Open Graph
-     // { property: 'fb:app_id', content: '123456789' },
-     // { property: 'og:title', content: 'Content Title' },
-     // with shorthand
-     { p: 'og:image', c: './assets/og_image.png' },
-     // ...
-   ]
- }
+ //  head: {
+ //   meta: [
+ //     // { name: 'application-name', content: 'Name of my application' },
+ //     // { name: 'description', content: 'A description of the page', id: 'desc' }, // id to replace intead of create element
+ //     // // ...
+ //     // // Twitter
+ //     // { name: 'twitter:title', content: 'Content Title' },
+ //     // // with shorthand
+ //     // { n: 'twitter:description', c: 'Content description less than 200 characters'},
+ //     // // ...
+ //     // // Google+ / Schema.org
+ //     // { itemprop: 'name', content: 'Content Title' },
+ //     // { itemprop: 'description', content: 'Content Title' },
+ //     // // ...
+ //     // // Facebook / Open Graph
+ //     // { property: 'fb:app_id', content: '123456789' },
+ //     // { property: 'og:title', content: 'Content Title' },
+ //     // with shorthand
+ //     { p: 'og:image', c: './assets/og_image.png' },
+ //     // ...
+ //   ]
+ // }
 }
 </script>
 
