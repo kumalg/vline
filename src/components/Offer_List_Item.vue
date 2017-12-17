6d81d9c85414c5c16de547cd6500b67c2f52be53
kumalg <kumalgfilms@gmail.com>
2017-12-17 17:54:24 +0100

    color changes

--- src/components/Offer_List_Item.vue
+++ src/components/Offer_List_Item.vue
@@ -67,7 +67,7 @@ export default {
     }
 
     &.marked {
-        background: linear-gradient(45deg,$colorPrimary 0,$colorPrimaryLighter 90%);
+        background: linear-gradient(45deg,$colorPrimaryDarker 0,$colorPrimaryLighter 90%);
         box-shadow: 0 8px 32px rgba($colorPrimary, .25);
         border-width: 0;
 
